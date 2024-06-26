c3fee9a8a952555984a0768359077c06ed2850ed
Mann-27 <108608856+Mann-27@users.noreply.github.com>
2024-06-26 10:27:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -6,49 +6,5 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log(err);
   });
 
-// const userSchema=new mongoose.Schema({
-//     firstName:{
-//         type:String,
-//         required:false,
-//     },
-//     lastName:{
-//         type:String,
-//         required:false,
-//     },
-//     email:{
-//         type:String,
-//         required:true,
-//         unique:true,
-//     },
-//     encryptedPassword:{
-//         type:String,
-//         required:true,
-//     },
-//     createdAt:{
-//         type:Date,
-//         default:Date.now,
-//     },
-// });
-// const taskSchema=new mongoose.Schema({
-//  title:{
-//     type:String,
-//     required:true,
-//  },
-//  description:{
-//     type:String,
-//     required:true,
-//  },
-//  dueDate:{
-//     type:Date,
-//     required:true,
-//  },
 
-//  status:{
-//     type:String,
-//     enum:["completed","pending"],
-//     default:"pending",
-//  },
-// });
-// const User=mongoose.model("User",userSchema);
-// const Task=mongoose.model("Task",taskSchema);
-// module.exports=Task;
+
